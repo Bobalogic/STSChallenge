08025e84895e870f77ee8e39d7717c1b943efc70
Bobalogic <afonsopalmeira@tecnico.uliboa.pt>
2023-07-20 14:53:06 +0100

    Cnnect and add to the db

--- mqtt.py
+++ mqtt.py
@@ -1,16 +1,35 @@
 import paho.mqtt.client as mqtt
 import sqlite3
 from enum import Enum
- 
+from datetime import datetime
+
+# Tipos de Sensores:
+#   - Temperatura = Temperature -> Celsius
+#   - CO2 = Gas Meter -> ppm
+#   - Presença = Presence -> 0 ou 1
+#   - Humidade = Water Meter -> L
+
+#TO DO:
+#   - Adicionar os sensores à base de dados
+#   - Adicionar os valores dos sensores à base de dados
+#   - Dizer à lia para mudar os nomes que usa no MQTT
+#   - Fazer a ligação à REST API usando o Flask
+
+#   -Usar a função get_current_timestamp() para obter o timestamp atual
+
+dbhub_io_url = "https://dbhub.io/Bobalogic/IoTroopers.db"
+
 class SensorUnits(Enum):
     Temperature = 'Celsius'
-    CO2 = 'ppm'
+    Gas = 'ppm'
     Presence = 'Presence'
+    Water_Meter = 'L'
+
 
 def initialize_db():
     try:
         # Connect to SQLite database - this will create a new database file if it doesn't exist
-        conn = sqlite3.connect("IoTroopers.db", check_same_thread=False)
+        conn = sqlite3.connect("dbhub_io_url", check_same_thread=False)
         cursor = conn.cursor()
 
         cursor.execute(""" SELECT count(name) FROM sqlite_master WHERE type='table' AND name='sensors' """)
@@ -57,6 +76,77 @@ def initialize_db():
         cursor.close()
         conn.close()
 
+def get_current_timestamp():
+    current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return current_timestamp
+
+def get_sensor_id_from_db():
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect("dbhub_io_url", check_same_thread=False)
+        cursor = conn.cursor()
+
+        # Execute a query to get the highest sensor ID from the "sensors" table
+        cursor.execute("SELECT MAX(id) FROM sensors")
+
+        # Fetch the result (the maximum sensor ID) from the query
+        highest_sensor_id = cursor.fetchone()[0]
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+        if highest_sensor_id is None:
+            return 0
+        # Return the highest sensor ID
+        return highest_sensor_id
+
+    except sqlite3.Error as e:
+        # Handle any potential errors that might occur during the database operation
+        print("Error: ", e)
+        return None
+
+def add_sensor_to_db(topics):
+    try:
+        new_sensor_id = get_sensor_id_from_db() + 1
+        conn = sqlite3.connect("dbhub_io_url", check_same_thread=False)
+        cursor = conn.cursor()
+
+        sensor_type = topics[4].replace(' ', '_')
+
+        cursor.execute("""
+            INSERT INTO sensors (id, name, type, office, building, room, units) VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (new_sensor_id, topics[3], topics[2], topics[0], topics[1], topics[4], SensorUnits[sensor_type].value))
+
+        conn.commit()
+
+    except sqlite3.Error as e:
+        # Handle any potential errors that might occur during the database operation
+        print("Error: ", e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def add_sensor_value_to_db(topics, value):
+    try:
+        conn = sqlite3.connect("dbhub_io_url", check_same_thread=False)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO sensor_values (sensor, timestamp, value) VALUES (?, ?, ?)
+        """, (get_sensor_id_from_db(), get_current_timestamp(), value))
+
+        conn.commit()
+    
+    except sqlite3.Error as e:
+        # Handle any potential errors that might occur during the database operation
+        print("Error: ", e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -65,14 +155,16 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe("SummerCampSTS/#")
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     #print(msg.topic+" --- "+str(msg.payload))
 
     topics = msg.topic.split('/')[1:]
-    print(topics)
-    print(msg.payload)
-
+    #print(topics) #representa os parametros do sensor
+    #print(msg.payload.decode('utf-8')) #isto é o valor do sensor
+    add_sensor_to_db(topics)
+    #add_sensor_value_to_db(topics, msg.payload)
     # Office, Building, Room, Sensor Name, Sensor
 
 
