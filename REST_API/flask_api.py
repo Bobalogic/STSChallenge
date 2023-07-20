1ca57fbc1d102cd121e709278a85f8d84d76327d
Manuel Santos <manel.a.d.santos@gmail.com>
2023-07-20 18:12:09 +0100

    Add sensor via API done

--- REST_API/flask_api.py
+++ REST_API/flask_api.py
@@ -11,43 +11,58 @@ app = Flask(__name__)
 # Request to get specific sensor data
 @app.route("/sensors/<sensorId>", methods=["GET"])
 def getSensor(sensorId):
+    # Connect to database
     db = sqlite3.connect("sensors.db")
     cur = db.cursor()
+    # Query database
     res = cur.execute(
         "SELECT value, timestamp FROM sensor_values "
         "WHERE sensor= " + str(sensorId) + " ORDER BY timestamp DESC LIMIT 10"
     )
     result = res.fetchall()
+    # Close database connection
     cur.close()
     db.close()
-
+    # Parse result into JSON
     entry = "["
     for read in result:
         subentry = {}
         subentry["value"] = read[0]
         subentry["timestamp"] = read[1]
         entry = entry + json.dumps(subentry) + ","
-
     entry = entry[:-1] + "]"
-
+    # Return JSON
     return entry
 
 
 # Request to insert new sensor data
 @app.route("/sensors", methods=["POST"])
 def addSensor():
+    # Get data from request
     data = request.get_json()
+    print("Adding sensor " + str(data["id"]))
+    # Connect to database
     db = sqlite3.connect("sensors.db")
     cur = db.cursor()
-    cur.execute(
-        "INSERT INTO sensors (id, name, type, office, building, room, units)"
-        "VALUES (:id, :name, :type, :office, :building, :room, :units)",
-        data,
-    )
-    db.commit()
-    cur.close()
-    db.close()
-    return "Adding sensor"
+    # Check if sensor exists
+    test = cur.execute("SELECT * FROM sensors WHERE id=" + str(data["id"]))
+    if test.fetchone() is None:
+        # Insert data into database
+        cur.execute(
+            "INSERT INTO sensors (id, name, type, office, building, room, units)"
+            "VALUES (:id, :name, :type, :office, :building, :room, :units)",
+            data,
+        )
+        db.commit()
+        # Close database connection
+        cur.close()
+        db.close()
+        return "Sensor " + str(data["id"]) + " added successfully"
+    else:
+        # Close database connection
+        cur.close()
+        db.close()
+        return "Sensor " + str(data["id"]) + " already exists"
 
 
 # Request to get sensor values from natural language query
