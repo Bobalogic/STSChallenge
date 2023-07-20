281e5100623c9763be33c7ef9708a776299e78b8
Manuel Santos <manel.a.d.santos@gmail.com>
2023-07-20 14:18:58 +0100

    REST API base done
    Missing database queries. 
    In progess

--- REST_API/flask_api.py
+++ REST_API/flask_api.py
@@ -1,19 +1,26 @@
 # By: Manuel Santos
 # Run: python .\flask_api.py
 
-from flask import Flask
+from flask import Flask, request
+
+# import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Server Works!"
+@app.route("/sensors/<sensorId>", methods=["GET"])
+def getSensor(sensorId):
+    return "Getting sensor " + sensorId + " data"
 
 
-@app.route("/greet")
-def say_hello():
-    return "Hello from Server"
+@app.route("/sensors", methods=["POST"])
+def addSensor():
+    return "Adding sensor"
+
+
+@app.route("/nlquery/<nlquery>", methods=["POST"])
+def getQuery(nlquery):
+    return "Query the system with prompt -> " + nlquery
 
 
 if __name__ == "__main__":
