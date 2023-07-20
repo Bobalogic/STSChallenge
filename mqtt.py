cbafc26d0b3621939fe9ed2405df06a987abdfd4
louykiller <lourencoccvc@gmail.com>
2023-07-20 12:14:01 +0100

    changes

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,11 @@
 import paho.mqtt.client as mqtt
 import sqlite3
+from enum import Enum
+ 
+class SensorUnits(Enum):
+    Temperature = 'Celsius'
+    CO2 = 'ppm'
+    Presence = 'Presence'
 
 def initialize_db():
     try:
@@ -40,18 +46,25 @@ def initialize_db():
         cursor.close()
         conn.close()
 
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("SummerCampSTS/Lisboa/#")
+    client.subscribe("SummerCampSTS/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    #print(msg.topic+" --- "+str(msg.payload))
+
+    topics = msg.topic.split('/')[1:]
+    print(topics)
+    print(msg.payload)
+
+    # Office, Building, Room, Sensor Name, Sensor
+
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
