25c2f2ebb20b523c8adf7d01361868e6c5d942c9
Manuel Santos <manel.a.d.santos@gmail.com>
2023-07-21 15:17:59 +0100

    .

--- RunMe.py
+++ RunMe.py
@@ -1,13 +1,5 @@
 import threading, subprocess
 
-# from Modules.chatGPT import *
-# from Modules.flask_api import *
-# from Modules.mqtt import *
-# from Modules.sensors import *
-# from Modules.frontend.app import *
-# from Modules.frontend.funcs import *
-# from Modules.frontend.index import *
-
 
 def generate_data():
     subprocess.call(["python", "Modules\\sensors.py"], shell=True)
@@ -22,8 +14,6 @@ def api():
 
 
 def frontend():
-    # subprocess.call(["cd", "Modules\\frontend"], shell=True)
-    # subprocess.call(["dir"], shell=True)
     subprocess.call(["streamlit", "run", "Modules\\frontend\\app.py"], shell=True)
 
 
@@ -31,15 +21,14 @@ if __name__ == "__main__":
     generate_data_thread = threading.Thread(target=generate_data)
     backend_thread = threading.Thread(target=backend)
     api_thread = threading.Thread(target=api)
-    # frontend_thread = threading.Thread(target=frontend)
+    frontend_thread = threading.Thread(target=frontend)
 
     generate_data_thread.start()
     backend_thread.start()
     api_thread.start()
-    # frontend_thread.start()
+    frontend_thread.start()
 
     generate_data_thread.join()
     backend_thread.join()
     api_thread.join()
-    # frontend_thread.join()
-    # frontend()
+    frontend_thread.join()
