b7a5f38ace5dde682a0b59dd55c02a35855217be
louykiller <lourencoccvc@gmail.com>
2023-07-21 12:09:10 +0100

    data validation

--- mqtt.py
+++ mqtt.py
@@ -3,18 +3,17 @@ import sqlite3
 from enum import Enum
 from datetime import datetime
 
-# TODO: Add what's left
+# type: units, min, max
 SensorUnits = {
-    'Temperature': 'Celsius',
-    'CO2 level': 'ppm',
-    'Presence': 'Presence',
-    'Water meter': 'Liters',
-    'Gas meter': 'Cubic meters',
-    'Noise': 'Decibels',
-    'Illuminance': 'Lux',
-    'Electricity meter': 'Watts',
-    'Number of people': 'Integer'
-    
+    'Temperature': ['Celsius', -10, 45],
+    'CO2 level': ['ppm', 100, 1000],
+    'Presence': ['Presence', 0, 1],
+    'Water meter': ['Liters', 200, 99999999],
+    'Gas meter': ['Cubic meters', 4000, 99999999],
+    'Noise': ['Decibels', 10, 60],
+    'Illuminance': ['Lux', 50, 1000],
+    'Electricity meter': ['Watts', 200000, 99999999], 
+    'Number of people': ['Integer', 0, 800]
 }
 
 databaseName = 'IoTroopers.db'
@@ -77,10 +76,7 @@ def initialize_db():
         cursor.close()
         conn.close()
 
-def get_current_timestamp():
-    current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return current_timestamp
-
+'''
 def get_sensor_id_from_db():
     try:
         # Connect to the SQLite database
@@ -106,7 +102,6 @@ def get_sensor_id_from_db():
         print("Error: ", e)
         return None
 
-'''
 def add_sensor_to_db(topics):
     new_sensor_id = get_sensor_id_from_db() + 1
     try:
@@ -128,6 +123,10 @@ def add_sensor_to_db(topics):
         conn.close()
     return new_sensor_id
 '''
+
+def get_current_timestamp():
+    current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return current_timestamp
 
 def add_sensor_value_to_db(id, value):
     try:
@@ -162,12 +161,29 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     # topics: Office, Building, Room, Name, Type, Units
     topics = msg.topic.split('/')[1:]
-    topics.append(SensorUnits.get(topics[-1]))
-    value = msg.payload.decode()
-    name = topics[3]
-
+    
     # Ignore keepalive messages
     if topics[2] != 'keepalive':
+        try:
+            # Get the match for the sensor unit and it's range of values
+            match = SensorUnits.get(topics[-1])
+            topics.append(match[0])
+            value = int(msg.payload.decode())
+            name = topics[3]
+            # Check if the value is in a valid range for it's type
+            if value < match[1] or value > match[2]:
+                print("Value (", value, ") out of range (", match[1], "-", match[2], ")")
+                return
+        # If the unit is not recognized
+        except TypeError as te:
+            print("Error in sensor:", ', '.join(map(str, topics[:-2])))
+            print("No known unit for", topics[-1])
+            return
+        # If it's not an integer value
+        except ValueError as ve:
+            print("Error in sensor:", ', '.join(map(str, topics[:-2])))
+            print("Not a number for value", value)
+            return
         # Check if the sensor is in memory
         global existingSensors
         id = existingSensors.get(name, -1)
