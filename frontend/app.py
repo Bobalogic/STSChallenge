d29718bad0e7a631062188dbef338e97cc46c3aa
Diogo Mealha <cswsis23-dm@criticalsoftware.com>
2023-07-21 12:00:29 +0100

    frontend apdt

--- frontend/app.py
+++ frontend/app.py
@@ -11,10 +11,19 @@ from plotly import graph_objs as go
 st.set_page_config(page_title="STS APP ")
 col1, col2 = st.columns([1,2])
 
-image = Image.open("logo.png")
+logo_1 = Image.open("logo_1.png")
+logo_2 = Image.open("logo_2.png")
+logo_3 = Image.open("logo_3.png")
 
-col1.image(image, channels="RGB", output_format="auto", use_column_width="auto")
-col2.title("CRITICAL INFORMATION CENTER")
+image = [logo_1, logo_2, logo_3]
+
+
+
+
+def banner(logo_index):
+	col1.image(image[logo_index], channels="RGB", output_format="auto", use_column_width="auto")
+	col2.title("CRITICAL INFORMATION CENTER")
+	col2.write("by IoTroopers")
 
 selected = option_menu(menu_title=None, 
 	options=["Add Sensor", "Sensor Data", "GPT"], 
@@ -22,7 +31,7 @@ selected = option_menu(menu_title=None,
 	orientation="horizontal")
 
 if selected == 'Add Sensor':
-
+	banner(1)
 	with st.form(key='form1', clear_on_submit=False):
 		#id_ = st.number_input("id: ", step=1, min_value=1) increments alone
 		name_ = st.text_input("name")
@@ -37,13 +46,13 @@ if selected == 'Add Sensor':
 	
 
 		if submit_btn:
-			if id_ and name_ and type_ and office_ and building_ and room_ and units_:
+			if name_ and type_ and office_ and building_ and room_ and units_:
 				headers={
 		    		'Content-type':'application/json', 
 		    		'Accept':'application/json'
 				}
 				json_data = {
-				    "id": id_,
+				    #"id": id_,
 				    "name": name_,
 				    "type": type_,
 				    "office": office_,
@@ -71,40 +80,44 @@ if selected == 'Add Sensor':
 
 
 if selected == 'Sensor Data':
+	banner(0)
 	sensor_id = st.number_input("id: ", step=1, min_value=1)	
 	if st.button("Send"):
 		response = requests.get("http://localhost:5000/sensors/{}".format(sensor_id))
 
 		json_data = json.loads(response.text)
+
+		print(json_data)
+
+		if len(json_data) == 0: # has no values
+			st.warning("Sensor {} has no values".format(sensor_id))
 		
-		st.subheader("Info:")
-		st.markdown("**Location :** {}".format(json_data['location']))
-		st.markdown("**Building :** {}".format(json_data['building']))
-		st.markdown("**Room :** {}".format(json_data['room']))
+		else:
+			st.subheader("Info:")
+			st.markdown("**Location :** {}".format(json_data['location']))
+			st.markdown("**Building :** {}".format(json_data['building']))
+			st.markdown("**Room :** {}".format(json_data['room']))
 
-		
+			
 
-		time = []
-		value = []
-		for i in range(10):
-			st.write("Value {} Timestamp: {}".format(json_data['data'][i]['value'], json_data['data'][i]['timestamp'][:-7]))
-			datetime_object = datetime.strptime(json_data['data'][i]['timestamp'][:-7], '%Y-%m-%d %H:%M:%S')
-			time.append(datetime_object)
-			value.append(json_data['data'][i]['value'])
+			time = []
+			value = []
+			for i in range(10):
+				st.write("Value {} Timestamp: {}".format(json_data['data'][i]['value'], json_data['data'][i]['timestamp'][:-7]))
+				datetime_object = datetime.strptime(json_data['data'][i]['timestamp'][:-7], '%Y-%m-%d %H:%M:%S')
+				time.append(datetime_object)
+				value.append(json_data['data'][i]['value'])
 
-		#np_info = np.array()
-		#a = pd.DataFrame(data=[[1, 4], [2, 4], [3, 7]], columns=["caralho", "miguel"])
-		#print(a)
-		#b = np.array([[1, 2], [2, 4], [3, 8]])
-		#st.line_chart(b)
-
-		fig = go.Figure()
-		fig.add_trace(go.Scatter(x=time, y=value, name="aaaa"))
-		fig.layout.update(title_text="Exemplo")
-		st.plotly_chart(fig)
+			
+			fig = go.Figure()
+			fig.add_trace(go.Scatter(x=time, y=value, name="aaaa"))
+			fig.layout.update(title_text=json_data['type'],
+				xaxis_title="Date", yaxis_title=json_data['units'])
+			st.plotly_chart(fig)	
 
 
 if selected == 'GPT':
+	banner(2)
 	prompt = st.text_input("enter your question")
 	if st.button("Send"):
 		response = requests.post("http://localhost:5000/nlquery/{}".format(prompt))
